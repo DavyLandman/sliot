28416a5dc98ff4e2477b00e2064157451883ba8e
Davy Landman <davy.landman@swat.engineering>
2019-11-09 14:34:37 +0100

    Finished first version of the siot connection

--- client-lib/src/secure_iot_connection.c
+++ client-lib/src/secure_iot_connection.c
@@ -10,40 +10,103 @@
 #define CRYPTO_ALIGNMENT __attribute__((aligned(4)))
 #define PACKED __attribute__((__packed__))
 
-static uint8_t dh_private[32] CRYPTO_ALIGNMENT;
-
-static void (*handshake_done)(uint8_t shared_key[32]) = NULL;
-static void (*sent_success)(void) = NULL;
-
-
-static void receive_handler(u8 *mac_addr, u8 *data, u8 len) {
-
-}
-
-static void send_handler(u8 *mac_addr, u8 status) {
-}
-
 
 struct PACKED signed_key_message {
     uint8_t signature[64];
     uint8_t public_key[32];
 };
 
-void siot_handshake_execute(struct siot_config *cfg, void (*done)(uint8_t shared_key[32])) {
-    os_get_random(dh_private, sizeof(dh_private));
+static uint8_t dh_private[32] CRYPTO_ALIGNMENT;
+static const struct siot_config *current_config;
 
+static void (*handshake_done)(uint8_t shared_key[32]) = NULL;
+static void (*send_success)(void) = NULL;
+static void (*send_failure)(void) = NULL;
+
+
+static void handle_dh_reply(const struct signed_key_message *msg) {
+    system_soft_wdt_feed();
+    if (crypto_check(msg->signature, current_config->server_long_term_public, msg->public_key, sizeof(msg->public_key))) {
+        system_soft_wdt_feed();
+        uint8_t shared_key[32];
+        crypto_key_exchange(shared_key, dh_private, msg->public_key);
+        system_soft_wdt_feed();
+        memset(dh_private, 0, sizeof(dh_private));
+        handshake_done(shared_key);
+        handshake_done = NULL;
+        system_soft_wdt_feed();
+        memset(shared_key, 0, sizeof(dh_private));
+    }
+}
+
+static void receive_handler(u8 *mac_addr, u8 *data, u8 len) {
+    if (handshake_done != NULL) {
+        // we are in a DH exchange, we dont check the sending mac, as it is to easy to fake
+        if (len != sizeof(struct signed_key_message) || current_config == NULL) {
+            // incorrect message received, exiting
+            return;
+        }
+        handle_dh_reply((const void*)data);
+    }
+}
+
+static void send_handler(u8 *mac_addr, u8 status) {
+    if (send_success != NULL && send_failure != NULL) {
+        if (status == 0) {
+            send_success();
+        }
+        else {
+            send_failure();
+        }
+        send_success = NULL;
+        send_failure = NULL;
+    }
+}
+
+
+void siot_handshake_execute(const struct siot_config *cfg, void (*done)(uint8_t shared_key[32])) {
+    while (os_get_random(dh_private, sizeof(dh_private)) != 0);
+
+    system_soft_wdt_feed();
     struct signed_key_message msg;
     crypto_key_exchange_public_key(msg.public_key, dh_private);
+    system_soft_wdt_feed();
     crypto_sign(msg.signature, cfg->long_term_secret, cfg->long_term_public, msg.public_key, sizeof(msg.public_key));
+    system_soft_wdt_feed();
 
+    current_config = cfg;
     handshake_done = done;
     esp_now_send(cfg->server_receiving_mac, (const u8*)((const void*)&msg), sizeof(msg));
 }
 
-void siot_send(struct siot_config *cfg, uint8_t shared_key[32], uint32_t counter, void* data, size_t length, void (*sent)(void));
+struct PACKED messageHeader {
+    uint16_t counter;
+    uint8_t nonce[24];
+    uint8_t mac[16];
+    uint8_t msg_size;
+};
+
+void siot_send(const struct siot_config *cfg, uint8_t shared_key[32], uint16_t counter, void* data, size_t length, void (*send)(void), void (*fail)(void)) {
+    uint8_t message[150] CRYPTO_ALIGNMENT; // max message size
+    if (length > (sizeof(message) - sizeof(struct messageHeader))) {
+        return;
+    }
+    struct messageHeader *header = (void *)message;
+    header->counter = counter;
+    header->msg_size = length;
+
+    while (os_get_random(header->nonce, sizeof(header->nonce)) != 0);
+    system_soft_wdt_feed();
+    crypto_lock_aead(header->mac, message + sizeof(struct messageHeader), shared_key, header->nonce, &header->counter, sizeof(header->counter), data, length);
+    system_soft_wdt_feed();
+
+    send_success = send;
+    send_failure = fail;
+    esp_now_send(cfg->server_receiving_mac, message, length + sizeof(struct messageHeader));
+}
 
 
-void siot_init_espnow(struct siot_config *cfg) {
+void siot_init_espnow(const struct siot_config *cfg) {
     // wakeup WIFI in in receiving mode
     wifi_set_opmode_current(SOFTAP_MODE);
     struct softap_config config;
--- client-lib/src/secure_iot_connection.h
+++ client-lib/src/secure_iot_connection.h
@@ -12,10 +12,10 @@ struct siot_config{
     uint8_t server_receiving_mac[6];
 };
 
-void siot_init_espnow(struct siot_config *cfg);
+void siot_init_espnow(const struct siot_config *cfg);
 
-void siot_handshake_execute(struct siot_config *cfg, void (*done)(uint8_t shared_key[32]));
+void siot_handshake_execute(const struct siot_config *cfg, void (*done)(uint8_t shared_key[32]));
 
-void siot_send(struct siot_config *cfg, uint8_t shared_key[32], uint32_t counter, void* data, size_t length, void (*sent)(void));
+void siot_send(const struct siot_config *cfg, uint8_t shared_key[32], uint16_t counter, void* data, size_t length, void (*send)(void), void (*fail)(void));
 
 #endif
